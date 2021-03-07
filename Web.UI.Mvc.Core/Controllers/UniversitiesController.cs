d507b2817fc4800e3674fcb0ad713c68ea525f47
Abdulrahman Albarghouth <albarghouth.a@std.izu.edu.tr>
2021-03-07 17:51:26 +0300

    V1.0
    Layuot

---
+++ Web.UI.Mvc.Core/Controllers/UniversitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.UI.Mvc.Core.Controllers
+{
+    public class UniversitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Single()
+        {
+            return View();
+        }
+
+    }
+}
