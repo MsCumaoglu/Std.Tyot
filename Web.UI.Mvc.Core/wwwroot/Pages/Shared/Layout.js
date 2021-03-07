696d97ae916bdded741ada12a7b38d8321e8063f
Mustafa Cumaoğlu <56727162+MsCumaoglu@users.noreply.github.com>
2021-03-07 18:55:11 +0300

    asd
    bir de benden

---
+++ Web.UI.Mvc.Core/wwwroot/Pages/Shared/Layout.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var width = $(window).width();
+    if (width > 1200) {
+        $("#main-nav").addClass(".col-lg-7");
+        $("#account-nav").addClass(".col-lg-2");
+    } else {
+        $("#main-nav").addClass(".col-md-8");
+        $("#account-nav").addClass(".col-md-1");
+    }
+});
